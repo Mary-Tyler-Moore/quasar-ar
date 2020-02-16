25d315dcb65a056a241aef1bd70c369d3ba840a6
Tyler Moore <tylermoore@Tylers-iMac.frontierlocal.net>
2020-02-16 07:54:21 -0500

    Install and Build

---
+++ dist/pwa-mat/precache-manifest.842cba0d871ed4da5895d5339b79c155.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc5051da8e65e87bb45",
+    "url": "css/1bdb053e.1e914325.css"
+  },
+  {
+    "revision": "8cc5051da8e65e87bb45",
+    "url": "js/1bdb053e.f8e93d7b.js"
+  },
+  {
+    "revision": "a327e04a1e28da5bc7d5",
+    "url": "css/32a5c660.6007b77e.css"
+  },
+  {
+    "revision": "a327e04a1e28da5bc7d5",
+    "url": "js/32a5c660.8830d798.js"
+  },
+  {
+    "revision": "a9ff913a3c17b881b663",
+    "url": "js/4b47640d.21cfc388.js"
+  },
+  {
+    "revision": "12edbeb8a79ef4e43b8d",
+    "url": "css/app.3a03a35f.css"
+  },
+  {
+    "revision": "12edbeb8a79ef4e43b8d",
+    "url": "js/app.f1930ce7.js"
+  },
+  {
+    "revision": "c8fcfccb626973f7494d",
+    "url": "js/vendor.76780bb5.js"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8a9a261c.woff2"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
+  },
+  {
+    "revision": "efb00e57304380d857603760600da5d5",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "de298eb8f513e31d10f74645ce05f74b",
+    "url": "statics/disponibilita.jpg"
+  },
+  {
+    "revision": "da2b422e2895b9b9c1782a71b6be9d63",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "82beed93749951cedbd19d3f7ff0d747",
+    "url": "statics/disponibilita2.jpg"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "2f9d7587924933c1912c342b190b53ca",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "20c6420ccf16a07d9d56538e30a6b524",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "a13b2bd5261e2df374128df1aab93001",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "ed12d10fd0407eea42ccaf7af7991146",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "66d7719a8ca03c1a591444c013802817",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "bb543bcda6d7af3e8daf1ab9f212e5df",
+    "url": "statics/ladder.png"
+  },
+  {
+    "revision": "1304cc08c221b7fb1d13007d4e9ef79b",
+    "url": "statics/labirinto.jpg"
+  },
+  {
+    "revision": "3bfd6d657d2212583591571da86b3692",
+    "url": "statics/Logo_festma.png"
+  },
+  {
+    "revision": "0229af6a55fc29ef14046c90563e64ff",
+    "url": "statics/marker/marker-frac2.png"
+  },
+  {
+    "revision": "fd61cdc8c108368b471ee58e4bb5af8d",
+    "url": "statics/marker/marker-frac1.png"
+  },
+  {
+    "revision": "2a7dde573277bc9918d5a67e7f3ca2a9",
+    "url": "statics/marker/marker-frac3.png"
+  },
+  {
+    "revision": "ac6543cd208d16963bff551927de575d",
+    "url": "statics/marker/pt-cite.patt"
+  },
+  {
+    "revision": "f5f3e3258c03c12a166b3c73f4aaf2e7",
+    "url": "statics/marker/marker-cite.png"
+  },
+  {
+    "revision": "af5968deb60831176e284cde34d88a54",
+    "url": "statics/marker/marker-frac4.png"
+  },
+  {
+    "revision": "3a0d1fdeb9f9cb0a780faa25e4a97b3e",
+    "url": "statics/marker/pt-frac1.patt"
+  },
+  {
+    "revision": "1b1b2f941ae6a1c0506e8bdd341325d1",
+    "url": "statics/marker/pt-frac2.patt"
+  },
+  {
+    "revision": "04cad844ab0132db02313fe0fbf4b2ed",
+    "url": "statics/marker/pt-frac3.patt"
+  },
+  {
+    "revision": "3020c8ac2c2872dec7741e5948520093",
+    "url": "statics/quasar-logo.png"
+  },
+  {
+    "revision": "d07cee4794d78b8c917096884302292b",
+    "url": "statics/quadrado-magico-kami.jpg"
+  },
+  {
+    "revision": "4e674ad9cc1f0f40f8d72803748818f8",
+    "url": "statics/quadrado-magico-black.jpg"
+  },
+  {
+    "revision": "cc5c6147f4cf3b5330d42a10669a9044",
+    "url": "statics/marker/pt-frac4.patt"
+  },
+  {
+    "revision": "ce36aef0c78b52a84d6bb99c3ea66386",
+    "url": "statics/the-window-ii.png"
+  },
+  {
+    "revision": "f23a484daa1e0b3937f7d3e7aff6c7cc",
+    "url": "statics/js/aframe.min.js"
+  },
+  {
+    "revision": "bc9647f9663a38d1dc410fe860cafa57",
+    "url": "statics/js/aframe-ar.js"
+  },
+  {
+    "revision": "1c42c9fe9c536ff5960fb098b3361e0f",
+    "url": "statics/peticov_animation.mp4"
+  },
+  {
+    "revision": "c736a84a219866d5d08bf9218f2d80ef",
+    "url": "index.html"
+  }
+];
